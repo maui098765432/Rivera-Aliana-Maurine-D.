f8629b10aeb940b2c6b440b1f3e615b715eae395
maui098765432 <126493196+maui098765432@users.noreply.github.com>
2024-11-30 19:56:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('slide-show');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('section');
+  hiddenElements.forEach((el) => observer.observe(el));
